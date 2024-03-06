552dc804b6e9ac38318bee251a833ef1fc737b12
Youchul Jeong <86945493+youchulJ@users.noreply.github.com>
2024-03-06 14:08:38 +0900

    Update #yolov5 (Google colab_EN).py

--- #yolov5 (구글 코랩 완성편).py
+++ #yolov5 (구글 코랩 완성편).py
@@ -1,31 +1,30 @@
-##구글 코랩에서 이용하는 방식으로 그대로 옯겨서 붙여사용하면 된다.1 
+## Copy and paste this code as is to use it in Google Colab.
 
-
-# 구글 드라이브 불러오기
+# Mount Google Drive
 from google.colab import drive
 drive.mount('/content/drive')
 
-# yolov5 불러오기
- !git clone https://github.com/ultralytics/yolov5 
+# Clone YOLOv5 repository
+!git clone https://github.com/ultralytics/yolov5 
 
 %cd yolov5 
-!pip install -r requirements.txt # install dependencies 
+!pip install -r requirements.txt # Install dependencies
 
-#data.yaml내의 내용 점검
+# Check contents of data.yaml
 %cat /content/dataset/data.yaml
 
-#pyyaml install 설정 (안하면 잘 안된다고 하여 넣음)
+# Install PyYAML for YAML parsing (required for proper functioning)
 !pip install -U PyYAML
 
-#yolov5의 위치로 이동하기 
+# Navigate to YOLOv5 directory
 %cd /content/yolov5
 
-#yolov5 트레이닝 하기 
+# Train YOLOv5
 !python train.py --img 640 --batch 30 --epochs 20 --data /content/dataset/data.yaml --cfg /content/yolov5/models/yolov5s.yaml --weights yolov5s.pt --name test1
 
-#or 각 내용은 img size, batch size, epoch 등등 그리고 yolov5 버전도 바꿔주면됨 이름설정 마찬가지
-!python train.py  --img 416 --batch 16 --epochs 50 --data /content/dataset/data.yaml --cfg ./models/yolov5s.yaml --weights yolov5s.pt --name styrofoam_yolov5s_results
+# You can customize parameters such as image size, batch size, epochs, YOLOv5 version, and name.
+!python train.py --img 416 --batch 16 --epochs 50 --data /content/dataset/data.yaml --cfg ./models/yolov5s.yaml --weights yolov5s.pt --name styrofoam_yolov5s_results
 
-# tesorboard를 통하여 그래프 확인하고 분석하기! 
+# Visualize and analyze training progress using TensorBoard
 %load_ext tensorboard
 %tensorboard --logdir /content/yolov5/runs
